1f4d83b67de55f646cfaa036a2e2d81eb95ce509
James Nowecki | Minstersoft <james.nowecki@minstersoft.co.uk>
2020-08-25 14:13:38 +0100

    changed access key

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,9 @@
 // get your own key from unsplash please 😇
-const KEY =
-    '?client_id=5f96323678d05ff0c4eb264ef184556868e303b32a2db88ecbf15746e6f25e02';
+// const KEY =
+//     '?client_id=5f96323678d05ff0c4eb264ef184556868e303b32a2db88ecbf15746e6f25e02';
+
+ const KEY =
+    '?client_id=C3vOnwR9uGoTWhRSIK46UgujfQK9O2KNMn9mn92RjyE';
 const URL = `https://api.unsplash.com/photos/`;
 
 const fetchImages = async page => {
